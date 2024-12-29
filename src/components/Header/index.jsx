f58841454a4c7944668a8a5cc12c09b13b439c1c
ShirishaKb <shirishakb@gmail.com>
2024-12-29 11:20:50 -0500

    updated asset links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react';
 
 const Header = () => {
     const [activeSection, setActiveSection] = useState('About Me');
-
-    const handleNavClick = (section) => {
-        setActiveSection(section);
-    };
-
     return (
         <header className="flex-row space-between px-1">
             <h1>Shirisha Korukonda Bhattaru</h1>
@@ -15,22 +10,22 @@ const Header = () => {
                     <li 
                         className={activeSection === 'About Me' ? 'active' : ''} 
                     >
-                        <a href="/">About Me</a>
+                        <a href="/" onClick={() => setActiveSection('About Me')}>About Me</a>
                     </li>
                     <li 
                         className={activeSection === 'Portfolio' ? 'active' : ''} 
                     >
-                        <a href="/portfolio">Portfolio</a>
+                        <a href="/portfolio" onClick={() => setActiveSection('Portfolio')}>Portfolio</a>
                     </li>
                     <li 
                         className={activeSection === 'Contact' ? 'active' : ''} 
                     >
-                        <a href="/contact">Contact</a>
+                        <a href="/contact" onClick={() => setActiveSection('Contact')}>Contact</a>
                     </li>
                     <li 
                         className={activeSection === 'Resume' ? 'active' : ''} 
                     >
-                       <a href="/resume">Resume</a>
+                       <a href="/resume" onClick={() => setActiveSection('Resume')}>Resume</a>
                     </li>
                 </ul>
             </nav>
