ba7b48a23a785ff85bb234320d5692e94317ecf8
ShirishaKb <shirishakb@gmail.com>
2024-12-29 22:14:37 -0500

    modified css

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,33 +1,33 @@
 import { NavLink } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
-    return (
-        <nav>
-            <ul className="flex-row">
-                <li>
-                    <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
-                        {capitalizeFirstLetter('About')}
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}>
-                        {capitalizeFirstLetter('Portfolio')}
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>
-                        {capitalizeFirstLetter('Contact')}
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/resume" className={({ isActive }) => (isActive ? 'active' : '')}>
-                        {capitalizeFirstLetter('Resume')}
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+              {capitalizeFirstLetter('About')}
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}>
+              {capitalizeFirstLetter('Portfolio')}
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>
+              {capitalizeFirstLetter('Contact')}
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/resume" className={({ isActive }) => (isActive ? 'active' : '')}>
+              {capitalizeFirstLetter('Resume')}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
